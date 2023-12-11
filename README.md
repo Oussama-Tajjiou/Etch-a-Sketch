96d04fcb15cc9c254da63df1ade963e79791b105
Oussama-Tajjiou <oussama.tajjiou@gmail.com>
2023-12-11 17:27:21 +0100

    Basic Etch a Sketch webpage

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple Etch a Sketch webpage.
---
+++ app.js
@@ -0,0 +1,75 @@
+function startingGrid(){
+    let container = document.querySelector("#container");
+    for(let i = 0; i < 16; i++){
+        let div = document.createElement('div');
+        div.className = 'big';
+        container.appendChild(div);
+        let j = 0;
+        do{
+            let divTwo = document.createElement('div')
+            divTwo.className = 'small';
+            div.appendChild(divTwo);
+            j++
+        }
+        while(j < 16);
+    }
+    coloringGrid();
+}
+
+startingGrid();
+
+function coloringGrid(){
+    let gridItems = document.querySelectorAll('.small');
+    gridItems = [...gridItems];
+    
+    gridItems.forEach((item) =>{
+        let gridItem = item;
+        gridItem.count = 1;
+        gridItem.addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor = '#6A6A6A';
+            e.target.count += 1;
+            e.target.style.opacity = 0.2 * e.target.count;
+
+        })
+    })
+};
+
+function gridSize(){
+    let btn = document.querySelector('.btn');
+    btn.addEventListener('click', (e) =>{
+        let grid = prompt('Enter a number from 16 to 64:');
+        let container = document.querySelector('#container');
+        let bigDiv = document.getElementsByClassName('big');
+        function removeAllChildNodes(){
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+            while (bigDiv.firstChild) {
+                bigDiv.removeChild(bigDiv.firstChild);
+            }
+        }
+        if(grid < 16 || grid > 64 || isNaN(grid) == true){
+            removeAllChildNodes();
+            startingGrid();
+        }
+        else{
+            removeAllChildNodes();
+            for(let i = 0; i < grid; i++){
+                let div = document.createElement('div');
+                div.className = 'big';
+                container.appendChild(div);
+                let j = 0;
+                do{
+                    let divTwo = document.createElement('div')
+                    divTwo.className = 'small';
+                    div.appendChild(divTwo);
+                    j++
+                }
+                while(j < grid);
+            }
+            coloringGrid();
+        }
+    })
+}
+
+gridSize();
